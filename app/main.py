630566eb655af9688c58a7c6f92d207dbf0186d2
jonatanalzate <jonatanalzate@hotmail.com>
2024-11-10 16:57:51 -0500

    Se publica el tercer endpoint y se hacen unos ajustes en el main para que el tercer endpoint se muestre en el navegador

--- app/main.py
+++ app/main.py
@@ -18,30 +18,8 @@ app = FastAPI()
 # Incluir los routers de los endpoints
 app.include_router(vehiculo_router)
 app.include_router(ruta_router)
+app.include_router(conductor_router)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
-    
-"""
-from fastapi import FastAPI
-from app.data.database import engine
-from app.domain.models.vehiculo import Base
-from app.domain.models.ruta import Base
-from app.presentation.api_vehiculo import router  # Importa el router de api.py
-from app.presentation.api_ruta import router  # Importa el router de api.py
-
-# Crear todas las tablas en la base de datos
-Base.metadata.create_all(bind=engine)
-
-# Inicializar la aplicación FastAPI
-app = FastAPI()
-
-# Incluir el router de los endpoints
-app.include_router(router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-"""
-    
